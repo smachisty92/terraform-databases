d9bad434ee7e8125c10039901d264844be23bb8e
csyed <smachisty92@gmail.com>
2022-03-11 01:17:14 +0530

    terraform

--- mongodb/ec2.tf
+++ mongodb/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "spot-instance" {
   instance_type = var.MONGODB_INSTANCE_TYPE
   ami           = data.aws_ami.ami.id
-  subnet_id = element(data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID,count.index)
+  subnet_id =  data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID[0]
   vpc_security_group_ids = [aws_security_group.allow-mongodb.id]
 }
 
