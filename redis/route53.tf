45cf824b7b0a4a38836fa36a8ffaa43473ba8238
csyed <smachisty92@gmail.com>
2022-03-11 16:06:14 +0530

    terraform

--- redis/route53.tf
+++ redis/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "redis" {
   name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONE_NAME}"
-  type    = "A"
+  type    = "CNAME"
   zone_id = data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONEID
   ttl = "300"
   records = [aws_elasticache_cluster.redis.cache_nodes[0].address]
