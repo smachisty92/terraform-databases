4d2ba9f39d1c4ee6c5ba1971a87282c7999db636
csyed <smachisty92@gmail.com>
2022-03-11 15:08:46 +0530

    terraform

--- redis/redis.tf
+++ redis/redis.tf
@@ -2,7 +2,7 @@ resource "aws_elasticache_cluster" "redis" {
   cluster_id = "reids-${var.ENV}"
   engine = "redis"
   node_type = "cache.t3.micro"
-  parameter_group_name = "default.redis6.2"
+  parameter_group_name = "default.redis6.x"
   engine_version = "6.x"
   port = 6379
   subnet_group_name = aws_elasticache_subnet_group.subnet-group.name
