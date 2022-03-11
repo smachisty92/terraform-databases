63964e5ad47724acea912a88445ee91fb5bae326
csyed <smachisty92@gmail.com>
2022-03-11 14:48:26 +0530

    terraform

---
+++ redis/redis.tf
@@ -0,0 +1,15 @@
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id = "reids-${var.ENV}"
+  engine = "redis"
+  node_type = "cache.t3.micro"
+  parameter_group_name = "default.redis6.2"
+  engine_version = "6.2"
+  port = 6379
+  subnet_group_name = aws_elasticache_subnet_group.subnet-group.name
+  security_group_ids = []
+}
+
+resource "aws_elasticache_subnet_group" "subnet-group" {
+  name       = "radis-${var.ENV}"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID
+}
---
+++ redis/sg.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow-redis" {
+  name        = "redis-${var.ENV}-sg"
+  description =  "redis-${var.ENV}-sg"
+  vpc_id = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "redis"
+    from_port        = 6379
+    to_port          = 6379
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "redis-${var.ENV}-sg"
+  }
+}
