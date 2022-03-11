10d827c075c33b3fe71471fc4d77f1569a193796
csyed <smachisty92@gmail.com>
2022-03-11 09:45:44 +0530

    terraform

--- mongodb/route53.tf
+++ mongodb/route53.tf
@@ -3,4 +3,5 @@ resource "aws_route53_record" "mongodb" {
   type    = "A"
   zone_id = data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONEID
   ttl = "300"
+  records = [aws_spot_instance_request.spot-instance.private_ip]
 }
