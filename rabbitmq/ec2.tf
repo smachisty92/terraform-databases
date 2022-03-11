b7cd8b27b1143e7f59a5fa7372d35a8cc98a32cb
csyed <smachisty92@gmail.com>
2022-03-11 10:28:57 +0530

    terraform

---
+++ rabbitmq/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_spot_instance_request" "spot-instance" {
+  instance_type = var.RABBITMQ_INSTANCE_TYPE
+  ami           = data.aws_ami.ami.id
+  subnet_id =  data.terraform_remote_state.vpc.outputs.PRIVATE-SUBNETS_ID[0]
+  vpc_security_group_ids = [aws_security_group.allow-rabbitmq.id]
+}
+
+resource "aws_ec2_tag" "tag" {
+  resource_id = aws_spot_instance_request.spot-instance.id
+  key         = "Name"
+  value       = "rabbitmq-${var.ENV}"
+}
+
+
+resource "null_resource" "ansible-apply" {
+  provisioner "remote-exec" {
+    connection {
+      host     = aws_spot_instance_request.spot-instance.private_ip
+      user     = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_USER"]
+      password = jsondecode(data.aws_secretsmanager_secret_version.latest.secret_string)["SSH_PASS"]
+    }
+
+    inline = [
+      "ansible-pull -U https://github.com/smachisty92/ansible roboshop-pull.yml -e COMPONENT=rabbitmq -e ENV=${var.ENV}"
+    ]
+  }
+}
