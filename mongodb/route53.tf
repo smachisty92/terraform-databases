ca6b721de631a2778f1d0b270a0ca1e60677acc8
csyed <smachisty92@gmail.com>
2022-03-11 09:38:59 +0530

    terraform

---
+++ mongodb/route53.tf
@@ -0,0 +1,6 @@
+resource "aws_route53_record" "mongodb" {
+  name    = "mongodb-${var.ENV}.${data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONE_NAME}"
+  type    = "A"
+  zone_id = data.terraform_remote_state.vpc.outputs.INTERNAL_HOSTED_ZONEID
+  ttl = "300"
+}
